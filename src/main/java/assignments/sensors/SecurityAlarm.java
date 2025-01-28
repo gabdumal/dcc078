53460d8ed536f9c8291b6bc28bb258b7a62c6d26
Gabriel Malosto <professional@gabdumal.com>
2025-01-28 20:46:31 -0300

    Define Sensor e SecurityAlarm

---
+++ src/main/java/assignments/sensors/SecurityAlarm.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Gabriel Malosto.
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+package assignments.sensors;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class SecurityAlarm
+        implements Observer {
+
+    @Override
+    public void update(
+            Observable sensor,
+            Object argumento
+    ) {
+
+    }
+
+}
