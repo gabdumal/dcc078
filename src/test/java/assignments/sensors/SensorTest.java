5ec04d4892321b66c27c97f106b413de1a4c6e9b
Gabriel Malosto <professional@gabdumal.com>
2025-01-29 12:40:45 -0300

    Cria testes para ArmedSensor

--- src/test/java/assignments/sensors/SensorTest.java
+++ src/test/java/assignments/sensors/SensorTest.java
@@ -13,12 +13,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SensorTest {
 
-    private Sensor sensor;
+    private Sensor        sensor;
     private SecurityAlarm securityAlarm;
 
     @BeforeEach
     public void setUp() {
-        this.sensor = new Sensor();
+        this.sensor        = new Sensor();
         this.securityAlarm = new SecurityAlarm("Cozinha");
         this.securityAlarm.register(this.sensor);
     }
@@ -57,6 +57,44 @@ public class SensorTest {
         this.sensor.setState(DisarmedSensor.getInstance());
 
         assertFalse(this.sensor.disarm());
+        assertEquals(DisarmedSensor.getInstance(), this.sensor.getState());
+        assertFalse(this.securityAlarm.isOn());
+    }
+
+    /* ArmedSensor */
+
+    @Test
+    public void shouldNotArmArmedSensor() {
+        this.sensor.setState(ArmedSensor.getInstance());
+
+        assertFalse(this.sensor.arm());
+        assertEquals(ArmedSensor.getInstance(), this.sensor.getState());
+        assertFalse(this.securityAlarm.isOn());
+    }
+
+    @Test
+    public void shouldTriggerArmedSensor() {
+        this.sensor.setState(ArmedSensor.getInstance());
+
+        assertTrue(this.sensor.trigger());
+        assertEquals(TriggeredSensor.getInstance(), this.sensor.getState());
+        assertTrue(this.securityAlarm.isOn());
+    }
+
+    @Test
+    public void shouldNotResetArmedSensor() {
+        this.sensor.setState(ArmedSensor.getInstance());
+
+        assertFalse(this.sensor.reset());
+        assertEquals(ArmedSensor.getInstance(), this.sensor.getState());
+        assertFalse(this.securityAlarm.isOn());
+    }
+
+    @Test
+    public void shouldDisarmArmedSensor() {
+        this.sensor.setState(ArmedSensor.getInstance());
+
+        assertTrue(this.sensor.disarm());
         assertEquals(DisarmedSensor.getInstance(), this.sensor.getState());
         assertFalse(this.securityAlarm.isOn());
     }
